3f02100687d20547512c19e6ce858c29fff02e2c
eyup170290064 <eyupakbaba1907@hotmail.com>
2022-05-29 16:01:09 +0300

    doctor login page completed and modified

--- doctorlogin.php
+++ doctorlogin.php
@@ -1,3 +1,59 @@
+<?php
+
+	include("include/connection.php");
+
+	if (isset($_POST['login'])){
+
+		$uname = $_POST['uname'];
+		$password = $_POST['pass'];
+
+		$error = array();
+
+		$q = "SELECT * FROM doctors WHERE username='$uname' AND password='$password'";
+		$qq = mysqli_query($connect,$q);
+
+		$row = mysqli_fetch_array($qq);
+
+		if(empty($uname)) { 
+			$error['login'] = "Enter Username";
+		} else if(empty($password)){
+			$error['login'] = "Enter Password";
+		} 
+		if (isset($row)) {
+			if($row['status'] == "Pending"){
+				$error['login'] = "Please wait for the admin to confirm";
+			} else if($row['status'] == "Rejected") {
+				$error['login'] = "You got rejected.Try again later!";
+			}	
+		}
+
+		if(count($error) == 0 ){
+			$query = "SELECT * FROM doctors WHERE username='$uname' AND password='$password'";
+
+			$res = mysqli_query($connect,$query);
+
+			if(mysqli_num_rows($res)){
+				echo "<script>alert('done')</script>";
+				$_SESSION['doctor'] = $uname;
+
+				//header("Location:")
+			}else {
+				echo "<script>alert('Invalid Account')</script>";
+			}
+		}
+
+	} 
+	if(isset($error['login'])){
+		$say = $error['login'];
+
+		$show = "<h5 class='text-center alert alert-danger'>$say</h5>";
+	}
+	else{
+		$show = "";
+	}
+
+?>
+
 <!DOCTYPE html>
 <html>
 	<head>
@@ -13,20 +69,24 @@
 		 		<div class="row">
 		 			<div class="col-md-3"></div>
 		 			<div class="col-md-6 jumbotron my-3">
-		 				<h5 class="text-center my-2"><p style="color:white">Doctor Login</p></h5>
+					 <form method="post" class="form-control">
+		 				<h5 class="text-center my-2"><b>Doctor Login</b></h5>
+						 	<div>
+								 <?php	echo $show; ?>
+							 </div>
 
-		 				<form method="post">
+		 				
 		 					<div class="form-group">
-		 						<label><p style="color:white">Username</p></label>
+		 						<label>Username</label>
 		 						<input type="text" name="uname" class="form-control" autocomplete="off" placeholder="Enter Username">
-		 					</div>
+		 					</div><br>
 		 					<div class="form-group">
-		 						<label><p style="color:white">Password</p></label>
+		 						<label>Password</label>
 		 						<input type="password" name="pass" class="form-control" autocomplete="off">
-		 					</div>
-		 					<input type="submit" name="login" class="btn btn-success" value="Login">
+		 					</div><br>
+		 					<input type="submit" name="login" class="btn btn-success" value="Login"><br>
 
-		 					<p style="color:white">I don't have an account</p><a href="apply.php">Apply Now!</a>
+		 					<p>I don't have an account. <a href="apply.php">Apply Now!</a></p>
 		 				</form>
 		 			</div>
 		 			<div class="col-md-3"></div>
