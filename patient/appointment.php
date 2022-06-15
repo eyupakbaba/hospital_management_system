b0c60f95b9f7d41a1fd1ce6681f944195bd5d38f
selamikadioglu1 <selami-kadioglu@windowslive.com>
2022-06-15 23:45:27 +0300

    modified appointment page

--- patient/appointment.php
+++ patient/appointment.php
@@ -31,18 +31,45 @@
                         $sel = mysqli_query($connect,$query);
                         $row = mysqli_fetch_array($sel);
 
+                        $firstname = $row['firstname'];
+                        $surname = $row['surname'];
+                        $gender = $row['gender'];
+                        $phone = $row['phone'];
+
+                        if(isset($_POST['book'])){
+
+                            $date = $_POST['date'];
+                            $sym = $_POST['sym'];
+
+                            if(empty($sym)){
+
+
+                            }else{
+
+                                $query = "INSERT INTO appointment(firstname,surname,gender,phone,appointment_date,symptoms,status,date_booked) VALUES('$firstname','$surname','$gender','$phone',$date,'$sym','Pending',NOW())";
+
+                                $res = mysqli_query($connect,$query);
+
+                                if($res){
+
+                                    echo "<script>alert('You have booked an appointment.')</script>";
+
+                                }
+                            }
+                        }
                         
                     ?>
-                    <div class="col-md-10">
+                    <div class="col-md-12">
                         <div class="row">
-                            <div class="col-md-3">
-                                <div class="col-md-6">
+                            <div class="col-md-3"></div>
+                                <div class="col-md-6 jumbotron">
                                     <form action="" method="post">
                                         <label for="">Appointment Date</label>
                                         <input type="date" name="date" class="form-control">
                                         <label for="">Symptoms</label>
                                         <input type="text" name="sym" class="form-control" autocomplete="off" placeholder="Enter Symtoms">
-                                        <input type="submit" name="book" class="btn btn-info" value="Book Appointment">
+
+                                        <input type="submit" name="book" class="btn btn-info my-2" value="Book Appointment">
                                     </form>
                                 </div>
                             </div>
